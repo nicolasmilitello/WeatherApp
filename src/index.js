106d6eb692b892da3ab12d5f3524863ef5113fa6
nicolasmilitello <nicolas.militello@hotmail.com>
2022-02-27 16:46:08 -0300

    mixed content error fixed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+axios.defaults.baseURL =
+  process.env.REACT_APP_API || "https://api.openweathermap.org";
 
 ReactDOM.render(
   <BrowserRouter>
