2bf0b2ff72e3318b121bc9b7b1960f244c47bc3c
nicolasmilitello <nicolas.militello@hotmail.com>
2022-02-27 18:21:52 -0300

    axios deleted}

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 import { BrowserRouter } from "react-router-dom";
-import axios from "axios";
+//import axios from "axios";
 // axios.defaults.baseURL =
 //   process.env.REACT_APP_API || "https://api.openweathermap.org";
 
